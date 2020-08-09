72c5b744eaf6704df9d9ddb850c72b2653b3e5d3
LesMiserables1 <liemandre20@gmail.com>
2020-08-09 16:32:02 +0000

    added config command with database functionality

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bot = require('./bot.js')
 
 const TOKEN_BOT = process.env.TOKEN_BOT
 const BASE_URI = `https://api.telegram.org/bot${TOKEN_BOT}`
-const URL = 'http://newsapi.org/v2/top-headlines?country=id&'+'apiKey='+process.env.NEWS_API_TOKEN
 
 let app = express()
 app.use(bodyParser.json())
@@ -16,7 +15,8 @@ app.post('/',(req,res)=>{
     res.send(body);
 })
 app.get('/',(req,res)=>{
-    res.send(URL)
+    bot.setCommands()
+    res.send('URL')
 })
 
 app.listen(3001)
--- model/db.js
+++ model/db.js
@@ -1,21 +1,69 @@
 const moongose = require('mongoose')
+var assert = require('assert');
 
-const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.qanws.mongodb.net/<dbname>?retryWrites=true&w=majority`
+const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.qanws.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 moongose.connect(dbUrl,{ useNewUrlParser: true,useUnifiedTopology: true })
-const roomSchema = new moongose.Schema({
+moongose.set('useFindAndModify', false);
+
+let roomSchema = new moongose.Schema({
     chat_id : Number,
+    command : String,
+    lang : {type : String, default : 'en'},
+    topic : {type : String, default : 'universal'}
+
+})
+
+const langSchema = new moongose.Schema({
+    lang : String
+})
+const topicSchema = new moongose.Schema({
     topic : String
 })
 
-const room = moongose.model('room',roomSchema)
+const Room = moongose.model('Room',roomSchema)
+const lang = moongose.model('Language',langSchema)
+const topic = moongose.model('Topic',topicSchema)
 
 let addRoom = async(data)=>{
-    await room.findOneAndUpdate({ chat_id : data.chat_id }, { topic : data.topic},{
-        upsert: true
+    await Room.findOneAndUpdate({ chat_id : data.chat_id }, {$set : { command : data.command}},{
+        upsert: true,
+        setDefaultsOnInsert: true
     })
 }
+let changeRoomLang = ()=>{
+
+}
+
+let changeRoomTopic = ()=>{
+    
+}
+
+let addLang = async(data)=>{
+    lang.create({lang : data.lang})
+}
+
+let addTopic = async(data)=>{
+    topic.create({topic : data.topic})
+}
+
+let getLang = async(data)=>{
+    let lang = Room.findOne({chat_id : data}).exec()
+    return lang
+}
+
+let getTopic = (data)=>{
+    let topic = Room.findOne({chat_id : data}).exec()
+    return topic
+}
+
 
 module.exports = {
-    addRoom : addRoom
-}
+    addRoom : addRoom,
+    getTopic : getTopic,
+    getLang : getLang,
+    addLang : addLang,
+    addTopic : addTopic,
+    changeRoomLang : changeRoomLang,
+    changeRoomTopic : changeRoomTopic
+} 
